bf9564a30a7d323ce68ce40b0d5340e1bba67709
Emile Keith <emilekeith3.0@gmail.com>
2017-09-30 11:02:48 -0500

    Adding basic styling to popup.html

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,1 +1,14 @@
+"use strict";
 
+const key = 'enabled';
+
+chrome.state.onChange.addListener(function(change, namespace) {
+    if(changes[key] !== undefined)
+    {
+      console.log(`The value of ${key} is ${changes[key]}`);
+    }
+});
+
+const setState = function(newState) {
+    chrome.storage.sync.set({key});
+};
